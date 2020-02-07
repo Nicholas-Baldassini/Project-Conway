64c20813308411b0e02e7cd3c381fd4ffa0f51cc
Ermaderm <owen.jory@gmail.com>
2020-02-07 12:53:06 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java
    
    Just making a print setup for testing purposes, not much to see

--- EOU.java
+++ EOU.java
@@ -2,6 +2,13 @@
 package ProjectConway;
 
 public class EOU {
+    
+    /*Print methods for test purposes
+    Pre: N/A
+    Post: N/A
+    Print a value
+    */
+    
     public static void println(String print){
         System.out.println(print);
     }public static void println(int print){
@@ -9,6 +16,8 @@ public class EOU {
     }public static void println(float print){
         System.out.println(print);
     }public static void println(double print){
+        System.out.println(print);
+    }public static void println(boolean print){
         System.out.println(print);
     }
     
@@ -19,6 +28,8 @@ public class EOU {
     }public static void print(float print){
         System.out.print(print);
     }public static void print(double print){
+        System.out.print(print);
+    }public static void print(boolean print){
         System.out.print(print);
     }
 }
