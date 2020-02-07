2fe956ea696eed1111ee7e9a8a36a54ed7951132
Ermaderm <owen.jory@gmail.com>
2020-02-07 11:34:54 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java

---
+++ EOU.java
@@ -0,0 +1,12 @@
+
+package ProjectConway;
+
+public class EOU {
+    public static void println(String print){
+        System.out.println(print);
+    }
+    
+    public static void print(String print){
+        System.out.print(print);
+    }
+}
