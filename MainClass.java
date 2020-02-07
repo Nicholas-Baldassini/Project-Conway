f371451d960b0b6e8e670b8ee48d67e5b4930fe1
Ermaderm <owen.jory@gmail.com>
2020-02-07 11:31:18 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,5 +6,9 @@ public class MainClass{
         for(int i =0; i<100; i++){
             System.out.println("Hello World");
         }
+        boolean githubworks = false;
+        while (githubworks) {
+            System.out.print("AAAAAAAAAAA");
+        }
     }
 }
