98aefc30a2f7f4136ab3f4eef196fcb3e6871332
Ermaderm <owen.jory@gmail.com>
2020-02-07 13:58:34 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java
    
    Boid functionality adding; get and save state functional

--- Boid.java
+++ Boid.java
@@ -8,6 +8,12 @@ public class Boid {
     int state;
     int territory;
     
+    /*
+    Constructor method
+    Pre: Previous boid has been updataed
+    Post: New boids data is saved
+    Save the instance variables of a new boid
+    */
     public Boid(int[] data){
         this.x = data[0];
         this.y = data[1];
@@ -15,7 +21,14 @@ public class Boid {
         this.territory = data[3];
     }
     
-    public void Save(){
-        
+    /*
+    SAVE method
+    Pre: Update has been called
+    Post: Data has been sent to the required list
+    Send instance variables to a new place
+    */
+    public int[] Save(){
+        int[] data = {this.x, this.y, this.state, this.territory};
+        return data;   
     }
 }
