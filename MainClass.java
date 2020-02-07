626e60f42e3a8a8e449f4b25e7aa390a0180684c
Ermaderm <owen.jory@gmail.com>
2020-02-07 11:57:08 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -4,11 +4,11 @@ package ProjectConway;
 public class MainClass{
     public static void main(String[] args){
         for(int i =0; i<100; i++){
-            System.out.println("Hello World");
+            EOU.println("Hello World");
         }
         boolean githubworks = false;
         while (githubworks) {
-            System.out.print("AAAAAAAAAAA");
+            EOU.print("");
         }
     }
 }
