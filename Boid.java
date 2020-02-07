9d6bb77a117a227093c838615c2c1f74d8d1db73
Ermaderm <owen.jory@gmail.com>
2020-02-07 13:47:28 -0500

    Merge origin/master
    Conflicts:
    	MainClass.java
    
    Boid functionality added

--- Boid.java
+++ Boid.java
@@ -3,4 +3,19 @@ package ProjectConway;
 
 public class Boid {
     
+    int x;
+    int y;
+    int state;
+    int territory;
+    
+    public Boid(int[] data){
+        this.x = data[0];
+        this.y = data[1];
+        this.state = data[2];
+        this.territory = data[3];
+    }
+    
+    public void Save(){
+        
+    }
 }
