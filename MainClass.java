b82cfef9c613883608cfa592a9706ad700430c75
salpap <salpap@10.76.5.57>
2020-02-10 10:18:24 -0500

    //Sal can commit

--- MainClass.java
+++ MainClass.java
@@ -3,9 +3,10 @@ package ProjectConway;
 
 public class MainClass{
     public static void main(String[] args){
-        for(int i =0; i<100; i++){
+        for(int i =0; i<10; i++){
             EOU.println(i);
         }
+        
         boolean githubworks = false;
         while (githubworks) {
             EOU.print("oeuf ");
