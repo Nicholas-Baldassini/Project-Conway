9dae86fb71826aaaedf8c88092735537e655ea11
Ermaderm <owen.jory@gmail.com>
2020-03-12 14:30:17 -0400

    All code is ready and working

--- Board.java
+++ Board.java
@@ -3,14 +3,16 @@ package ProjectConway;
 
 public class Board {
     Boid[] stuff;
+    Rule[] rules;
     
-    public Board(Boid[] data){
+    public Board(Boid[] data, Rule[] rules){
         this.stuff = data;
+        this.rules = rules;
     }
     
-    public void massUpdate(Rule[] rules){
+    public void massUpdate(){
         for(Boid boid:this.stuff){
-            boid.update(this.stuff, rules);
+            boid.update(this.stuff, this.rules);
         }
         for(Boid boid:this.stuff){
             boid.fix();
