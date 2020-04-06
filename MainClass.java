bebf44ed36d5906ed354d10527a4fcde091076b1
salpap <salpap@10.76.5.57>
2020-04-06 13:45:24 -0400

    Made the MainClass display a (blank) screen

--- MainClass.java
+++ MainClass.java
@@ -1,8 +1,25 @@
 //this is a comment. i wrote it
 package ProjectConway;
 
-public class MainClass{
+import javax.swing.*;
+import java.awt.*;
+
+public class MainClass extends JFrame{
     public static void main(String[] args){
+        
+        //creating the screen
+        JFrame screen = new JFrame("Conway Main Menue"); // creates the screen variable, sets the title to what is in the brackets
+        screen.setSize(400, 500); // sets the size of the screen
+        screen.setVisible(true); // displays the screen
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // tells the program to stop running when you click x
+        //setting the color
+        Color customColor = new Color(0, 0, 0);
+        screen.getContentPane().setBackground(Color.GRAY);
+        
+        //play button
+        
+        
+        /*
         for(int i =0; i<11; i++){
             EOU.println(i);
         }
@@ -11,5 +28,6 @@ public class MainClass{
         while (swingworks) {
             EOU.print("oeuf ");
         }
+*/
     }
 }
